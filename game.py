04a83d8b48f605987e117f3ebd152ea3cd29bdc5
tobiascr <tobiascrw@gmail.com>
2019-10-31 04:05:21 +0100

    new file:   game.py 	modified:   chess.py 	modified:   test.py

---
+++ game.py
@@ -0,0 +1,15 @@
+
+class Game:
+
+    def __init__(self, FEN_string):
+        pass
+        
+        
+    def board_value(square):
+        """Return "k", "K", "p" etc f there is a piece on the square,
+        and None if there is no piece. Square can be "a1", "b5" etc.
+        """
+
+    def legal_move(self, move):
+        """Return
+
