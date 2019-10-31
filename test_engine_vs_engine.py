22a040a80fc163d61fd31bb052d6a2628b9ef7ca
tobiascr <tobiascrw@gmail.com>
2019-10-31 04:05:58 +0100

    new file:   test_engine_vs_engine.py

---
+++ test_engine_vs_engine.py
@@ -0,0 +1,22 @@
+
+import chess
+
+# The engine play against itself.
+game_state = chess.GameState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -")
+print(game_state)
+for n in range(200):
+    move = chess.computer_move(game_state)
+    game_state.make_move(move)
+    print(game_state)
+    print(move)
+    print("Position value: ", game_state.value)
+    if game_state.check_mate():
+        if game_state.white_to_play:
+            print("Black win")
+        else:
+            print("White win")
+        print("Check mate")
+        break
+    if game_state.stale_mate():
+        print("Stale mate")
+        break
