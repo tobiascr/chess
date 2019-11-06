000984082c13491e6ab2349c403cb56ca4930597
tobiascr <tobiascrw@gmail.com>
2019-11-06 06:55:14 +0100

    modified:   game.py 	modified:   test.py

--- game.py
+++ game.py
@@ -17,7 +17,9 @@ Pieces are represented as:
 import engine
 
 class Game:
-
+    """It is intended that this class should function as an interface for the engine
+    and everything else in this module.
+    """
     def __init__(self, FEN_string):
         self.FEN_string = FEN_string
         self.FEN_string_history = []
@@ -32,14 +34,99 @@ class Game:
         """Return values like "K", "k", "N" etc if there is a piece at the given
         position and None if there is none. The position is given as "a1", "b5" etc.
         """
-        return self.board.value(position)
+        return self.board.get_value(position)
 
     def legal(self, UCI_format_move):
         """Return true iff the move is legal"""
         return UCI_format_move in self.legal_moves
 
     def make_move(self, UCI_format_move):
-        pass
+        from_position = UCI_format_move[0:2]
+        to_position = UCI_format_move[2:4]
+        piece_moved = self.board.get_value(from_position)
+        non_en_passant_capture = self.board.get_value(to_position) != None
+
+        # Update the board.
+        self.board.make_move(UCI_format_move)
+
+        # Add the current FEN string to history.
+        self.FEN_string_history.append(self.FEN_string)
+
+        # Update the FEN string.
+        FEN_fields = self.FEN_string.split(" ")
+        self.FEN_string = self.board.FEN_string_first_part()
+
+        if FEN_fields[1] == "w":
+            self.FEN_string += " b "
+            black_made_last_move = False
+        else:
+            self.FEN_string += " w "
+            black_made_last_move = True
+
+        castling_availability = FEN_fields[2]
+
+        # If white castling.
+        if UCI_format_move == "e1c1" or UCI_format_move == "e1g1":
+            castling_availability = castling_availability.replace("K","")
+            castling_availability = castling_availability.replace("Q","")
+        # If black castling.
+        elif UCI_format_move == "e8c8" or UCI_format_move == "e8g8":
+            castling_availability = castling_availability.replace("k","")
+            castling_availability = castling_availability.replace("q","")
+        else:
+            if "K" in castling_availability:
+                if from_position == "e1" or from_position == "h1" or to_position == "h1":
+                    castling_availability = castling_availability.replace("K","")
+            if "Q" in castling_availability:
+                if from_position == "e1" or from_position == "a1" or to_position == "a1":
+                    castling_availability = castling_availability.replace("Q","")
+            if "k" in castling_availability:
+                if from_position == "e8" or from_position == "h8" or to_position == "h8":
+                    castling_availability = castling_availability.replace("k","")
+            if "q" in castling_availability:
+                if from_position == "e8" or from_position == "a8" or to_position == "a8":
+                    castling_availability = castling_availability.replace("q","")
+
+        if castling_availability == "":
+            castling_availability = "-"
+        self.FEN_string += castling_availability + " "
+
+        # En passant.
+        en_passant_target_square = "-"
+        if piece_moved == "P":
+            if UCI_format_move[1] == "2" and UCI_format_move[3] == "4":
+                print("hi")
+                en_passant_target_square = UCI_format_move[0] + "3"
+        if piece_moved == "p":
+            if UCI_format_move[1] == "7" and UCI_format_move[3] == "5":
+                en_passant_target_square = UCI_format_move[0] + "6"
+        self.FEN_string += en_passant_target_square + " "
+
+        # Half move clock. This is the number of halfmoves since the last
+        # capture or pawn advance.
+        if len(FEN_fields) <= 4:
+            half_move_clock = 0
+        else:
+            half_move_clock = int(FEN_fields[4])
+
+        if piece_moved == "P" or piece_moved == "p" or non_en_passant_capture:
+            half_move_clock = 0
+        else:
+            half_move_clock += 1
+
+        self.FEN_string += str(half_move_clock) + " "
+
+        # Full move number.
+        if len(FEN_fields) <= 5:
+            full_move_number = 1
+        else:
+            full_move_number = int(FEN_fields[5])
+
+        if black_made_last_move:
+            full_move_number += 1
+
+        self.FEN_string += str(full_move_number)
+
 
     def computer_move(self):
         """Return a move computed by the engine."""
@@ -99,6 +186,7 @@ class Board:
         FEN_fields = FEN_string.split(" ")
         FEN_rows = FEN_fields[0].split("/")
         FEN_rows.reverse()
+
         for row in range(8):
             col = 0
             for char in FEN_rows[row]:
@@ -122,7 +210,7 @@ class Board:
                " | " + " | ".join(row(7-i)) + " |" + "\n   +" + "---+"*8
                for i in range(8)]) + "\n     a   b   c   d   e   f   g   h"
 
-    def value(self, position):
+    def get_value(self, position):
         """Return values like "K", "k", "N" etc if there is a piece at the given
         position and None if there is none. The position is given as "a1", "b5" etc.
         """
@@ -130,8 +218,96 @@ class Board:
         row = (int(position[1]) - 1)
         return self.rows[row][col]
 
+    def set_value(self, position, value):
+        """The position is given as "a1", "b5" etc. Value can be K", "n", None etc."""
+        col = "abcdefgh".find(position[0]) 
+        row = (int(position[1]) - 1)
+        self.rows[row][col] = value
+
     def make_move(self, UCI_format_move):
-        pass
+
+        # Promotions
+        if len(UCI_format_move) == 5:
+            from_position = UCI_format_move[0:2]
+            to_position = UCI_format_move[2:4]
+            to_piece = UCI_format_move[4]
+            # Convert to_piece to upper case if the player is white.
+            if self.get_value(from_position) == "P":
+                    to_piece = to_piece.upper()
+            self.set_value(from_position, None)
+            self.set_value(to_position, to_piece)
+            return
+
+        # Castlings
+        if UCI_format_move == "e1c1":
+            self.set_value("a1", None)
+            self.set_value("b1", None)
+            self.set_value("c1", "K")
+            self.set_value("d1", "R")
+            self.set_value("e1", None)
+            return
+        if UCI_format_move == "e1g1":
+            self.set_value("e1", None)
+            self.set_value("f1", "R")
+            self.set_value("g1", "K")
+            self.set_value("h1", None)
+            return
+        if UCI_format_move == "e8c8":
+            self.set_value("a8", None)
+            self.set_value("b8", None)
+            self.set_value("c8", "k")
+            self.set_value("d8", "r")
+            self.set_value("e8", None)
+            return
+        if UCI_format_move == "e8g8":
+            self.set_value("e8", None)
+            self.set_value("f8", "r")
+            self.set_value("g8", "k")
+            self.set_value("h8", None)
+            return
+
+        # En passants
+        from_position = UCI_format_move[0:2]
+        piece = self.get_value(from_position)
+        if piece == "P" or piece == "p":
+            to_position = UCI_format_move[2:4]
+            if from_position[0] != to_position[0]:
+                if self.get_value(to_position) == None:
+                    capture_position = to_position[0] + from_position[1]
+                    self.set_value(from_position, None)
+                    self.set_value(to_position, piece)
+                    self.set_value(capture_position, None)
+                    return
+
+        # Ordinary moves
+        from_position = UCI_format_move[0:2]
+        to_position = UCI_format_move[2:4]
+        piece = self.get_value(from_position)
+        self.set_value(from_position, None)
+        self.set_value(to_position, piece)
+
+    def FEN_string_first_part(self):
+        """Use the data in this class to produce the first part of a FEN string
+        that describe the placement of the pieces."""
+        def convert_to_FEN_row(row):
+            FEN_row = ""
+            empty_position_count = 0
+            for value in row:
+                if value == None:
+                    empty_position_count += 1
+                else:
+                    if empty_position_count > 0:
+                        FEN_row += str(empty_position_count)
+                        empty_position_count = 0
+                    FEN_row += value
+            if empty_position_count > 0:
+                FEN_row += str(empty_position_count)
+            return FEN_row
+
+        return "/".join([convert_to_FEN_row(self.rows[i]) for i in range(7,-1,-1)])
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -13,6 +13,9 @@ def print_board(game):
 
 # Castling tests
 #FEN_string = "3k4/3p4/1q6/8/8/8/8/4K2R w K -"
+#FEN_string = "r3k2r/8/8/8/8/8/8/R3K2R w KQkq -"
+#FEN_string = "r3k2r/8/8/8/8/8/8/R3K2R w KQ -"
+#FEN_string = "r3k2r/8/8/8/8/8/8/R3K2R b KQkq -"
 #game_state = engine.GameState("3k4/3p4/8/8/8/4n3/8/4K2R w K -")
 #game_state = engine.GameState("3k4/2np4/8/8/8/8/8/4K2R w K -")
 #game_state = engine.GameState("3k4/2np4/8/8/8/8/8/4K1BR w K -") 
@@ -35,11 +38,22 @@ def print_board(game):
 #game_state = engine.GameState("8/6b1/8/8/8/k7/P7/R3K3 w Q -")
 #game_state = engine.GameState("r3k3/p5b1/K7/8/8/8/P6B/R6Q b q -")
 #game_state = engine.GameState("4k2r/7p/7K/8/8/8/1B6/8 b k -")
+#FEN_string = "4k3/8/8/8/2Q2n2/7b/8/R3K3 w Q -"
+#FEN_string = "r3k3/8/8/8/5n2/7b/8/3NK3 b q -"
+#FEN_string = "4k2r/8/8/8/5n2/7b/8/3NK3 b k -"
 
 # En passant
 #FEN_string = "8/8/8/2Pp3k/8/8/3K4/8 w - d6 0 2"
-#game_state = engine.GameState("7Q/1k6/8/2q5/4Pp2/8/K7/8 b - e3 0 1")
-#game_state = engine.GameState("")
+#FEN_string = "7Q/1k6/8/2q5/4Pp2/8/K7/8 b - e3 0 1"
+#FEN_string = "4k2r/2p5/8/8/Pp6/8/5P2/R3K3 b Qk a3"
+
+# Trigger en passant.
+#FEN_string = "4k3/8/8/8/4p3/8/5P2/4K3 w - -"
+#FEN_string = "4k3/8/4p3/8/8/8/2P5/4K3 w - -"
+#FEN_string = "4k3/p2p2p1/8/8/8/8/2P5/4K3 b - -"
+#FEN_string = "8/8/8/2Pp3k/8/8/3K1P2/8 w - d6 0 2"
+#FEN_string = "4k2r/2p5/8/8/8/8/P4P2/R3K3 w Qk -"
+#FEN_string = "4k2r/2p5/8/8/1p6/8/P4P2/R3K3 w Qk -"
 
 # Captures
 #FEN_string = "8/2k3r1/8/8/8/8/2K3R1/8 w - -"
@@ -53,11 +67,36 @@ def print_board(game):
 # Forks
 FEN_string = "4k3/8/8/2K4b/5n2/8/8/2Q5 b - -"
 
+# Promotions
+#FEN_string = "4k2r/P7/8/8/5n2/7b/8/3NK3 w K -"
+#FEN_string = "4k2r/P7/8/8/5n2/7b/6p1/3NK3 b - -"
+
 #Other tests
 #game_state = engine.GameState("8/4P3/8/8/7k/8/3r4/2K5 w - -")
 #game_state = engine.GameState("8/4P3/8/8/3R3k/8/3r4/2K5 w - -")
 #FEN_string = "8/4P3/8/7k/3R4/8/3K2p1/8 b - -"
+#FEN_string = "8/8/8/2Pp3k/8/6n1/8/4K2R b K -"
+#FEN_string = "7b/8/8/2Pp3k/8/8/8/R3K3 b Q -"
+#FEN_string = "r3k2r/8/8/2PBB3/8/8/8/R3K3 w Qkq -"
+#FEN_string = "4k3/1p6/8/8/8/8/1Q4P1/4K3 w - -"
+#FEN_string = "4k3/1p6/1Q6/8/8/8/6P1/4K3 b - - 10"
+#FEN_string = "4k2n/1p6/6R1/8/8/8/1Q4P1/4K3 w - -"
+#FEN_string = "4k2n/1p6/6R1/8/8/1Q6/6P1/4K3 b - - 40"
+#FEN_string = "4k3/1p3n2/6R1/8/8/1Q6/6P1/4K3 w - - 41"
+#FEN_string = "4k3/1p3n2/6R1/8/6P1/1Q6/8/4K3 b - g3 0"
+#FEN_string = "rnbqk1nr/pppp1ppp/1b2p3/8/3PP3/3B1N2/PPP2PPP/RNBQK2R b KQkq - 2 4"
+FEN_string = "rnbqk2r/pppp1ppp/1b2pn2/8/3PP3/3B1N2/PPP2PPP/RNBQK2R w KQkq - 3 5"
 
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
+#FEN_string = ""
 #FEN_string = ""
 #FEN_string = ""
 #FEN_string = ""
@@ -82,4 +121,11 @@ print("Possible moves:", engine.legal_moves_UCI(FEN_string))
 if engine.legal_moves_UCI(FEN_string):
     move = engine.computer_move_UCI(FEN_string)
     print("Best move found by engine:", move)
+
+    #game.make_move(move)
+    game.make_move("e1g1")
+    print(game)
+    print(game.FEN_string)
+
+
 
