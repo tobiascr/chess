7f853f9c173e7683d29c960bbc518cdb873cf1a1
tobiascr <tobiascrw@gmail.com>
2023-01-25 15:11:41 +0100

    modified:   test_engine_vs_engine.py

--- test_engine_vs_engine.py
+++ test_engine_vs_engine.py
@@ -1,6 +1,6 @@
 
 from game import Game
-
+import time
 import engine as engine1
 import test_engine as engine2
 
@@ -11,18 +11,34 @@ number_of_games = int(input("Number of games: "))
 engine1_wins = 0
 engine2_wins = 0
 draws = 0
+engine1_time = 0
+engine2_time = 0
+engine1_max_move_time = 0
+engine2_max_move_time = 0
+engine1_number_of_moves = 0
+engine2_number_of_moves = 0
 engine1_name = "engine.py"
 engine2_name = "test_engine.py"
 
 for n in range(1, number_of_games + 1):
     game = Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -")
+
     engine1_to_play = (n % 2) == 1
     print(game)
     while(True):
+        start_time = time.perf_counter()
         if engine1_to_play:
             move = engine1.computer_move_UCI(game.FEN_string)
+            engine1_number_of_moves += 1
+            move_time = time.perf_counter() - start_time
+            engine1_time += move_time
+            engine1_max_move_time = max(move_time, engine1_max_move_time)
         else:
             move = engine2.computer_move_UCI(game.FEN_string)
+            engine2_number_of_moves += 1
+            move_time = time.perf_counter() - start_time
+            engine2_time += move_time
+            engine2_max_move_time = max(move_time, engine2_max_move_time)
         engine1_to_play = not engine1_to_play
         game.make_move(move)
         print(game)
@@ -66,4 +82,12 @@ for n in range(1, number_of_games + 1):
             print_info()
             break
         print_info()
+
+print(engine1_name + " time used for all games:", engine1_time, "s")
+print(engine2_name + " time used for all games:", engine2_time, "s")
+print(engine1_name + " max move time:", engine1_max_move_time, "s")
+print(engine2_name + " max move time:", engine2_max_move_time, "s")
+print(engine1_name + " average move time:", engine1_time/engine1_number_of_moves, "s")
+print(engine2_name + " average move time:", engine2_time/engine2_number_of_moves, "s")
+
 
